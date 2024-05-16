6583cc14ffd3bf2634667af1c203cff762986ad3
HarshiniManthena <manthenaharshini@gmail.com>
2024-05-15 20:43:38 -0400

    commiting

---
+++ Addition.java
@@ -0,0 +1,14 @@
+package Operations;
+
+public class Addition {
+  static int a = 2;
+  static int b = 3;
+  
+  static int result = a + b;
+  public static void main(String[] args) {
+
+    System.out.println(result);
+    
+  }
+  
+}
